2e0bf3018419c0647b56b0fb2e0d1ea97e60c2da
Supuni Sithara <sitharabandara1997@gmail.com>
2024-09-07 12:47:28 +0530

    Added services

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import { useEffect } from 'react';
 import Aos from 'aos';
 import Header from './components/Header/Header';
-import Footer from './components/Header/Footer/Footer';
+import Footer from './components/Footer/Footer';
 import Hero from './components/UI/Hero';
+import Services from './components/UI/Services';
+
 function App() {
 
   useEffect(() => {
@@ -15,6 +17,7 @@ function App() {
       <Header />
       <main>
         <Hero/>
+        <Services/>
       </main>
       <Footer />
     </>

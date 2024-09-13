18799f41d376662120710a0c9b354954c6e7f617
Supuni Sithara <sitharabandara1997@gmail.com>
2024-09-13 12:05:04 +0530

    Updated styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,49 +1,97 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
 
 const Header = () => {
-    return (
-        <header className='w-full h-[80px] leading-[80px] flex items-center'>
-            <div className='container'>
-                <div className='flex items-center justify-between'>
-                    {/*Logo start*/}
-                    <div className='flex items-center gap-[10px]'>
-                        <span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center'>
-                            M
-                        </span>
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
 
-                        <div className='leading-[20px]'>
-                            <h1 className='text-xl text-smallTextColor font-[700]'>Supuni</h1>
-                            <p className='text-smallTextColor font-[500] text-[14px]'>personal</p>
-                        </div>
-                    </div>
-                    {/*Logo end*/}
+  useEffect(() => {
+    const stickyHeaderFunc = () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header');
+      } else {
+        headerRef.current.classList.remove('sticky__header');
+      }
+    };
 
-                    {/*Menu start*/}
-                    <div className='menu'>
-                        <ul className='flex items-center gap-10'>
-                            <li><a className=' text-smallTextColor font-[700]' href='#about'>About</a></li>
-                            <li><a className=' text-smallTextColor font-[700]' href='#services'>Services</a></li>
-                            <li><a className=' text-smallTextColor font-[700]' href='#portfolio'>Portfolio</a></li>
-                            <li><a className=' text-smallTextColor font-[700]' href='#contact'>Contact</a></li>
-                        </ul>
-                    </div>
-                    {/*Menu end*/}
+    window.addEventListener('scroll', stickyHeaderFunc);
 
-                    {/*Menu right start*/}
-                    <div className='flex items-center gap-4'>
-                    <button className='flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-300'>
-                    <i class="ri-send-plane-line"></i>Let's Talk
-                    </button>
+    return () => {
+      window.removeEventListener('scroll', stickyHeaderFunc);
+    };
+  }, []);
 
-                    <span className='text-xl text-smallTextColor md:hidden cursor-pointer'>
-                    <i class="ri-menu-line"></i>
-                    </span>
-                    </div>
-                    {/*Menu right end*/}
-                </div>
+  const handleClick = (e) => {
+    e.preventDefault();
+    const targetAttr = e.target.getAttribute('href');
+    const location = document.querySelector(targetAttr).offsetTop;
+
+    window.scrollTo({
+      top: location - 80,
+      left: 0,
+    });
+  };
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu');
+
+  return (
+    <header ref={headerRef} className='w-full h-[80px] leading-[80px] flex items-center'>
+      <div className='container'>
+        <div className='flex items-center justify-between'>
+          {/* Logo start */}
+          <div className='flex items-center gap-[10px]'>
+            <span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center'>
+              M
+            </span>
+
+            <div className='leading-[20px]'>
+              <h1 className='text-xl text-smallTextColor font-[700]'>Supuni</h1>
+              <p className='text-smallTextColor font-[500] text-[14px]'>personal</p>
             </div>
-        </header>
-    )
-}
+          </div>
+          {/* Logo end */}
 
-export default Header
+          {/* Menu start */}
+          <div ref={menuRef} className='menu'>
+            <ul className='flex items-center gap-10'>
+              <li>
+                <a onClick={handleClick} className='text-smallTextColor font-[700]' href='#about'>
+                  About
+                </a>
+              </li>
+              <li>
+                <a onClick={handleClick} className='text-smallTextColor font-[700]' href='#services'>
+                  Services
+                </a>
+              </li>
+              <li>
+                <a onClick={handleClick} className='text-smallTextColor font-[700]' href='#portfolio'>
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a onClick={handleClick} className='text-smallTextColor font-[700]' href='#contact'>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* Menu end */}
+
+          {/* Menu right start */}
+          <div className='flex items-center gap-4'>
+            <button className='flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-300'>
+              <i className='ri-send-plane-line'></i>Let's Talk
+            </button>
+
+            <span onClick={toggleMenu} className='text-xl text-smallTextColor md:hidden cursor-pointer'>
+              <i className='ri-menu-line'></i>
+            </span>
+          </div>
+          {/* Menu right end */}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
